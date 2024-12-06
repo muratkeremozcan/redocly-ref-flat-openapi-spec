854c0bac342e0405289780e135d7ce8fe7de22ca
Murat Ozcan <muratkerem@gmail.com>
2024-12-06 12:35:51 -0600

    doc: redocly api docs on gh page

--- cypress/index.d.ts
+++ cypress/index.d.ts
@@ -16,40 +16,6 @@ declare global {
         nameOrFlag: string | boolean | (() => boolean),
         cb?: () => void
       ): Chainable<Subject>
-
-      /**
-       * Validates the response body against the provided schema.
-       *
-       * @param schema - OpenAPI schema object to validate against.
-       * @param options - Endpoint and method information for the schema, with optional path and status.
-       *
-       * @example
-       * ```js
-       * cy.validateSchema(schema, {
-       *   endpoint: '/movies',
-       *   method: 'POST'
-       * })
-       * ```
-       *
-       * You can optionally specify `path` and `status`:
-       *
-       * @example
-       * ```js
-       * cy.validateSchema(schema, {
-       *   endpoint: '/movies',
-       *   method: 'POST',
-       *   status: 201 // Defaults to 200 if not provided
-       * })
-       * ``` */
-      validateSchema(
-        schema: OpenAPIV3_1.Document,
-        options: {
-          path?: string
-          endpoint: string
-          method: string
-          status?: string | number
-        }
-      ): Chainable<Subject>
     }
   }
 }
